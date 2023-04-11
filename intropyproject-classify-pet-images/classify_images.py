057d7b89859860af2f1bfabcbb7ae7f43f102066
Natasha Estrada <natashaestrada@gmail.com>
2023-04-11 13:36:53 -0700

    Update classify_images.py
    Updated to complete TODOs 0,1&2

--- intropyproject-classify-pet-images/classify_images.py
+++ intropyproject-classify-pet-images/classify_images.py
@@ -1,68 +1,133 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/classify_images.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                 
-# REVISED DATE: 
-# PURPOSE: Create a function classify_images that uses the classifier function 
-#          to create the classifier labels and then compares the classifier 
-#          labels to the pet image labels. This function inputs:
-#            -The Image Folder as image_dir within classify_images and function 
-#             and as in_arg.dir for function call within main. 
-#            -The results dictionary as results_dic within classify_images 
-#             function and results for the functin call within main.
-#            -The CNN model architecture as model within classify_images function
-#             and in_arg.arch for the function call within main. 
-#           This function uses the extend function to add items to the list 
-#           that's the 'value' of the results dictionary. You will be adding the
-#           classifier label as the item at index 1 of the list and the comparison 
-#           of the pet and classifier labels as the item at index 2 of the list.
+# */AIPND-revision/intropyproject-classify-pet-images/check_images.py
 #
+# TODO 0: Add your information below for Programmer & Date Created.                                                                             
+# PROGRAMMER: Natasha E. Estrada
+# DATE CREATED:  04/07/2023                                
+# REVISED DATE: 04/11/2023
+# PURPOSE: Classifies pet images using a pretrained CNN model, compares these
+#          classifications to the true identity of the pets in the images, and
+#          summarizes how well the CNN performed on the image classification task. 
+#          Note that the true identity of the pet (or object) in the image is 
+#          indicated by the filename of the image. Therefore, your program must
+#          first extract the pet image label from the filename before
+#          classifying the images using the pretrained CNN model. With this 
+#          program we will be comparing the performance of 3 different CNN model
+#          architectures to determine which provides the 'best' classification.
+#
+# Use argparse Expected Call with <> indicating expected user input:
+#      python check_images.py --dir <directory with images> --arch <model>
+#             --dogfile <file that contains dognames>
+#   Example call:
+#    python check_images.py --dir pet_images/ --arch vgg --dogfile dognames.txt
 ##
-# Imports classifier function for using CNN to classify images 
-from classifier import classifier 
 
-# TODO 3: Define classify_images function below, specifically replace the None
-#       below by the function definition of the classify_images function. 
-#       Notice that this function doesn't return anything because the 
-#       results_dic dictionary that is passed into the function is a mutable 
-#       data type so no return is needed.
-# 
-def classify_images(images_dir, results_dic, model):
-    """
-    Creates classifier labels with classifier function, compares pet labels to 
-    the classifier labels, and adds the classifier label and the comparison of 
-    the labels to the results dictionary using the extend function. Be sure to
-    format the classifier labels so that they will match your pet image labels.
-    The format will include putting the classifier labels in all lower case 
-    letters and strip the leading and trailing whitespace characters from them.
-    For example, the Classifier function returns = 'Maltese dog, Maltese terrier, Maltese' 
-    so the classifier label = 'maltese dog, maltese terrier, maltese'.
-    Recall that dog names from the classifier function can be a string of dog 
-    names separated by commas when a particular breed of dog has multiple dog 
-    names associated with that breed. For example, you will find pet images of
-    a 'dalmatian'(pet label) and it will match to the classifier label 
-    'dalmatian, coach dog, carriage dog' if the classifier function correctly 
-    classified the pet images of dalmatians.
-     PLEASE NOTE: This function uses the classifier() function defined in 
-     classifier.py within this function. The proper use of this function is
-     in test_classifier.py Please refer to this program prior to using the 
-     classifier() function to classify images within this function 
-     Parameters: 
-      images_dir - The (full) path to the folder of images that are to be
-                   classified by the classifier function (string)
-      results_dic - Results Dictionary with 'key' as image filename and 'value'
-                    as a List. Where the list will contain the following items: 
-                  index 0 = pet image label (string)
-                --- where index 1 & index 2 are added by this function ---
-                  NEW - index 1 = classifier label (string)
-                  NEW - index 2 = 1/0 (int)  where 1 = match between pet image
-                    and classifer labels and 0 = no match between labels
-      model - Indicates which CNN model architecture will be used by the 
-              classifier function to classify the pet images,
-              values must be either: resnet alexnet vgg (string)
-     Returns:
-           None - results_dic is mutable data type so no return needed.         
-    """
-    None 
+# Imports python modules
+from time import time, sleep
+
+# Imports print functions that check the lab
+from print_functions_for_lab_checks import *
+
+# Imports functions created for this program
+from get_input_args import get_input_args
+from get_pet_labels import get_pet_labels
+from classify_images import classify_images
+from adjust_results4_isadog import adjust_results4_isadog
+from calculates_results_stats import calculates_results_stats
+from print_results import print_results
+
+# Main program function defined below
+def main():
+    # TODO 0: Measures total program runtime by collecting start time
+    start_time = time()
+    
+    sleep(75)
+    
+    
+    # TODO 1: Define get_input_args function within the file get_input_args.py
+    # This function retrieves 3 Command Line Arugments from user as input from
+    # the user running the program from a terminal window. This function returns
+    # the collection of these command line arguments from the function call as
+    # the variable in_arg
+    in_arg = get_input_args()
+
+    # Function that checks command line arguments using in_arg  
+    check_command_line_arguments(in_arg)
+
+    
+    # TODO 2: Define get_pet_labels function within the file get_pet_labels.py
+    # Once the get_pet_labels function has been defined replace 'None' 
+    # in the function call with in_arg.dir  Once you have done the replacements
+    # your function call should look like this: 
+    #             get_pet_labels(in_arg.dir)
+    # This function creates the results dictionary that contains the results, 
+    # this dictionary is returned from the function call as the variable results
+    results = get_pet_labels(in_arg.dir)
+
+    # Function that checks Pet Images in the results Dictionary using results    
+    check_creating_pet_image_labels(results)
+
+
+    # TODO 3: Define classify_images function within the file classiy_images.py
+    # Once the classify_images function has been defined replace first 'None' 
+    # in the function call with in_arg.dir and replace the last 'None' in the
+    # function call with in_arg.arch  Once you have done the replacements your
+    # function call should look like this: 
+    #             classify_images(in_arg.dir, results, in_arg.arch)
+    # Creates Classifier Labels with classifier function, Compares Labels, 
+    # and adds these results to the results dictionary - results
+    classify_images(None, results, None)
+
+    # Function that checks Results Dictionary using results    
+    check_classifying_images(results)    
+
+    
+    # TODO 4: Define adjust_results4_isadog function within the file adjust_results4_isadog.py
+    # Once the adjust_results4_isadog function has been defined replace 'None' 
+    # in the function call with in_arg.dogfile  Once you have done the 
+    # replacements your function call should look like this: 
+    #          adjust_results4_isadog(results, in_arg.dogfile)
+    # Adjusts the results dictionary to determine if classifier correctly 
+    # classified images as 'a dog' or 'not a dog'. This demonstrates if 
+    # model can correctly classify dog images as dogs (regardless of breed)
+    adjust_results4_isadog(results, None)
+
+    # Function that checks Results Dictionary for is-a-dog adjustment using results
+    check_classifying_labels_as_dogs(results)
+
+
+    # TODO 5: Define calculates_results_stats function within the file calculates_results_stats.py
+    # This function creates the results statistics dictionary that contains a
+    # summary of the results statistics (this includes counts & percentages). This
+    # dictionary is returned from the function call as the variable results_stats    
+    # Calculates results of run and puts statistics in the Results Statistics
+    # Dictionary - called results_stats
+    results_stats = calculates_results_stats(results)
+
+    # Function that checks Results Statistics Dictionary using results_stats
+    check_calculating_results(results, results_stats)
+
+
+    # TODO 6: Define print_results function within the file print_results.py
+    # Once the print_results function has been defined replace 'None' 
+    # in the function call with in_arg.arch  Once you have done the 
+    # replacements your function call should look like this: 
+    #      print_results(results, results_stats, in_arg.arch, True, True)
+    # Prints summary results, incorrect classifications of dogs (if requested)
+    # and incorrectly classified breeds (if requested)
+    print_results(results, results_stats, None, True, True)
+    
+    # TODO 0: Measure total program runtime by collecting end time
+    end_time = time()
+    
+    # TODO 0: Computes overall runtime in seconds & prints it in hh:mm:ss format
+    tot_time = end_time - start_time
+    print("\n** Total Elapsed Runtime:",
+          str(int((tot_time/3600)))+":"+str(int((tot_time%3600)/60))+":"
+          +str(int((tot_time%3600)%60)) )
+    
+
+# Call to main function to run the program
+if __name__ == "__main__":
+    main()
