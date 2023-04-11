4a0240d2743a039e60ced4b3facd85647ae8a266
Natasha Estrada <natashaestrada@gmail.com>
2023-04-11 13:50:42 -0700

    Update check_images.py
    Update to finish TODO3

--- intropyproject-classify-pet-images/check_images.py
+++ intropyproject-classify-pet-images/check_images.py
@@ -77,7 +77,11 @@ def main():
     #             classify_images(in_arg.dir, results, in_arg.arch)
     # Creates Classifier Labels with classifier function, Compares Labels, 
     # and adds these results to the results dictionary - results
-    classify_images(None, results, None)
+    
+    
+    # Call to classify_images function
+    classify_images(in_arg.dir, results, in_arg.arch)
+
 
     # Function that checks Results Dictionary using results    
     check_classifying_images(results)    
