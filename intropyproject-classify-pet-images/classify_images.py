7ad9df810cc5579d09dcaa7d216a3bc384c70b18
Natasha Estrada <natashaestrada@gmail.com>
2023-04-11 13:46:41 -0700

    Update classify_images.py
    Update TODO 3

--- intropyproject-classify-pet-images/classify_images.py
+++ intropyproject-classify-pet-images/classify_images.py
@@ -29,6 +29,8 @@ from classifier import classifier
 #       results_dic dictionary that is passed into the function is a mutable 
 #       data type so no return is needed.
 # 
+from classifier import classifier
+
 def classify_images(images_dir, results_dic, model):
     """
     Creates classifier labels with classifier function, compares pet labels to 
@@ -45,17 +47,12 @@ def classify_images(images_dir, results_dic, model):
     a 'dalmatian'(pet label) and it will match to the classifier label 
     'dalmatian, coach dog, carriage dog' if the classifier function correctly 
     classified the pet images of dalmatians.
-     PLEASE NOTE: This function uses the classifier() function defined in 
-     classifier.py within this function. The proper use of this function is
-     in test_classifier.py Please refer to this program prior to using the 
-     classifier() function to classify images within this function 
-     Parameters: 
+    Parameters: 
       images_dir - The (full) path to the folder of images that are to be
                    classified by the classifier function (string)
       results_dic - Results Dictionary with 'key' as image filename and 'value'
                     as a List. Where the list will contain the following items: 
                   index 0 = pet image label (string)
-                --- where index 1 & index 2 are added by this function ---
                   NEW - index 1 = classifier label (string)
                   NEW - index 2 = 1/0 (int)  where 1 = match between pet image
                     and classifer labels and 0 = no match between labels
@@ -65,4 +62,26 @@ def classify_images(images_dir, results_dic, model):
      Returns:
            None - results_dic is mutable data type so no return needed.         
     """
-    None 
+    # Loop through each key-value pair in the results dictionary
+    for key, value in results_dic.items():
+        # Use the classifier function to create the classifier label for the image
+        image_path = images_dir + key
+        classifier_label = classifier(image_path, model).lower().strip()
+
+        # Add the classifier label to the results dictionary
+        value.append(classifier_label)
+
+        # Compare the pet label and the classifier label
+        pet_label = value[0]
+        match = 0
+        if pet_label in classifier_label:
+            # Check if the pet label is a whole word in the classifier label
+            pet_words = pet_label.split()
+            classifier_words = classifier_label.split(',')
+            for pet_word in pet_words:
+                if pet_word in classifier_words:
+                    match = 1
+                    break
+
+        # Add the match result to the results dictionary
+        value.append(match)
