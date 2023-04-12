9d89c0341f04f93ecdcc582b8581810fea9ab0b4
Natasha Estrada <natashaestrada@gmail.com>
2023-04-12 16:56:32 -0700

    Update adjust_results4_isadog.py
    Final version

--- intropyproject-classify-pet-images/adjust_results4_isadog.py
+++ intropyproject-classify-pet-images/adjust_results4_isadog.py
@@ -4,7 +4,7 @@
 #                                                                             
 # PROGRAMMER: Natasha E. Estrada
 # DATE CREATED: 04/12/2023                                
-# REVISED DATE: 
+# REVISED DATE: 04/12/2023
 # PURPOSE: Create a function adjust_results4_isadog that adjusts the results 
 #          dictionary to indicate whether or not the pet image label is of-a-dog, 
 #          and to indicate whether or not the classifier image label is of-a-dog.
